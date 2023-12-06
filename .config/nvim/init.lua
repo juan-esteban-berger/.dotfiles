916543b8c4bda7c23261bbcbc1ca424144a05237
juan-esteban-berger <juanestebanberger@gmail.com>
2023-12-06 08:25:36 -0600

    Updates to nvim config

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -1,9 +1,8 @@
 -- bootstrap lazy.nvim, LazyVim and your plugins
 require("config.lazy")
 
-vim.cmd("colorscheme koehler")
-
 vim.api.nvim_set_keymap("v", "<C-c>", '"+y', { noremap = true, silent = true })
 vim.api.nvim_set_keymap("n", "<C-c>", '"+yy', { noremap = true, silent = true })
 vim.api.nvim_set_keymap("v", "<", "<gv", { noremap = true, silent = true })
 vim.api.nvim_set_keymap("v", ">", ">gv", { noremap = true, silent = true })
+vim.wo.number = true
