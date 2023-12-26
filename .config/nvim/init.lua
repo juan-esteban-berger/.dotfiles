be800b7a64e9cbbb61589fae2533a1acacb7ca65
juan-esteban-berger <juanestebanberger@gmail.com>
2023-12-26 08:11:07 -0600

    quick update

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -1,6 +1,15 @@
 -- bootstrap lazy.nvim, LazyVim and your plugins
 require("config.lazy")
 
+vim.api.nvim_set_keymap("v", "<C-S-C>", '"+y', { noremap = true, silent = true 
+})
+vim.api.nvim_set_keymap("n", "<C-S-C>", '"+yy', { noremap = true, silent = true 
+})
+vim.api.nvim_set_keymap("v", "<C-S-V>", '"+p', { noremap = true, silent = true 
+})
+vim.api.nvim_set_keymap("n", "<C-S-V>", '"+p', { noremap = true, silent = true 
+})
+
 vim.api.nvim_set_keymap("v", "<C-c>", '"+y', { noremap = true, silent = true })
 vim.api.nvim_set_keymap("n", "<C-c>", '"+yy', { noremap = true, silent = true })
 vim.api.nvim_set_keymap("v", "<", "<gv", { noremap = true, silent = true })
